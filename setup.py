d0c4a4ee7dd8810d827480a53197336095e5a8da
Nikolay Kim <fafhrd91@gmail.com>
2011-05-27 21:33:48 -0500

    set version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version='0'
+version='0.1'
 
 install_requires = [
     'setuptools',
