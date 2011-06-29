c1e6cd1cf89d0f3f8c61ba39d6060bbdae63f7b5
Nikolay Kim <fafhrd91@gmail.com>
2011-06-29 14:16:40 -0500

    added change log; prepare for release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version='0.6dev'
+version='0.6'
 
 setup(name='memphis.config',
       version=version,
