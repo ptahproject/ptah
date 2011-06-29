c536bf650e29447190b43bc2afa435f0a038fa22
Nikolay Kim <fafhrd91@gmail.com>
2011-06-29 14:17:28 -0500

    back to dev

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version='0.6'
+version='0.7dev'
 
 setup(name='memphis.config',
       version=version,
