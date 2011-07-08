23504e129ca34914d6a6857c751b9b9c0af9884d
Nikolay Kim <fafhrd91@gmail.com>
2011-07-08 14:06:01 -0500

    prepare release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version='0.7'
+version='0.8'
 
 setup(name='memphis.view',
       version=version,
