4ed2adc2b8a23d2097992cf934765409b41f2918
Nikolay Kim <fafhrd91@gmail.com>
2011-07-08 14:09:19 -0500

    back to dev

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version='0.8'
+version='0.9dev'
 
 setup(name='memphis.view',
       version=version,
