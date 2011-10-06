1c1cc214c921724aff5674f266982fbc2f6b5a77
Nikolay Kim <fafhrd91@gmail.com>
2011-10-06 15:06:49 -0500

    prepare package

---
+++ README.txt
@@ -0,0 +1,9 @@
+Memphis config
+==============
+
+This package provides:
+
+- two stage registration subsystem for zope component registry.
+
+- global application settings subsystem allow to load/save settings 
+  from file, alow it allows to introspect all registerd settings in application
---
+++ setup.py
@@ -0,0 +1,52 @@
+""" Setup for memphis package """
+import sys, os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+version='1.0dev'
+
+setup(name='memphis',
+      version=version,
+      description="",
+      long_description=(
+          'Detailed Documentation\n' +
+          '======================\n'
+          + '\n\n' +
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI'],
+      author='Nikolay Kim',
+      author_email='fafhrd91@gmail.com',
+      url='https://github.com/ptahproject/memphis/',
+      license='BSD-derived',
+      packages = find_packages(),
+      install_requires = ['setuptools',
+                          'colander',
+                          'iso8601',
+                          'chameleon',
+                          'pyramid',
+                          'pytz',
+                          'simplejson',
+                          'zope.interface >= 3.8.0',
+                          ],
+      include_package_data = True,
+      zip_safe = False,
+      entry_points = {
+        'memphis': ['package = memphis'],
+        'console_scripts': [
+            'settings = memphis.config.commands:settingsCommand',
+            ],
+        },
+      )
