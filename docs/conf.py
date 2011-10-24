ca055d5bf8e3fd2d08770fb2822c058d22bfab96
alan runyan <runyaga@runyaga.com>
2011-10-24 14:12:17 -0500

    dont get people excited about version number

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ project = 'ptah'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '1.0'
+version = '0.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
