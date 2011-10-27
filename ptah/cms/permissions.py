fcdb74714b91ec6a4b68d0b1553f295e1a0b1cba
Nikolay Kim <fafhrd91@gmail.com>
2011-10-27 12:29:11 -0500

    permission id consistency #8

--- ptah/cms/permissions.py
+++ ptah/cms/permissions.py
@@ -1,7 +1,7 @@
 import ptah
 
-View = ptah.Permission('ptah:view', 'View')
-AddContent = ptah.Permission('ptah-cms:Add', 'Add content')
-ModifyContent = ptah.Permission('ptah-cms:Edit', 'Modify content')
-DeleteContent = ptah.Permission('ptah-cms:Delete', 'Delete content')
-ShareContent = ptah.Permission('ptah-cms:Share', 'Share content')
+View = ptah.Permission('ptah:View', 'View')
+AddContent = ptah.Permission('ptah-cms:AddContent', 'Add content')
+ModifyContent = ptah.Permission('ptah-cms:EditContent', 'Modify content')
+DeleteContent = ptah.Permission('ptah-cms:DeleteContent', 'Delete content')
+ShareContent = ptah.Permission('ptah-cms:ShareContent', 'Share content')
