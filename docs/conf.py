3da20ff165e4fb393c54981c7e7ab66e1c823520
alan runyan <runyaga@gmail.com>
2012-01-11 02:20:57 -0600

    we are nearly at version 0.3 also now that database schemas are not automagically created; add FAQ entry.  examples have been updated to bootstrap schema and initial database load.

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ project = 'ptah'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.2'
+version = '0.3'
 # The full version, including alpha/beta/rc tags.
 release = version
 
