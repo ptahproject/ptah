7da1c346c82256c2aad3f6dce564338c30813e76
Nikolay Kim <fafhrd91@gmail.com>
2012-01-20 14:23:01 -0600

    fix version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import logging
 import multiprocessing # atexit exception
 from setuptools import setup, find_packages
 
-version='0.3.1'
+version='0.3.2'
 
 install_requires = ['setuptools',
                     'alembic',
