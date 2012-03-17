a8297916f8d5c8ff24378c3de8a6e722c516d13e
Nando Florestan <nandoflorestan@gmail.com>
2012-03-17 09:44:36 -0300

    Print statements broke my Python 3.3.

--- ptah/sqlautils.py
+++ ptah/sqlautils.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+
+'''
+'''
+
+from __future__ import (absolute_import, division, print_function,
+    unicode_literals)  # Avoid breaking Python 3
+
 import uuid
 from threading import local
 
@@ -28,8 +36,8 @@ def reset_session():
     _zte = ZopeTransactionExtension()
     _session = orm.scoped_session(orm.sessionmaker(extension=[_zte]))
 
-
 _sa_session = local()
+
 
 class transaction(object):
 
@@ -160,10 +168,10 @@ try:
     class LZ4Serializer(object):
 
         def dumps(self, val):
-            print '========================'
-            print (json.dumps(val),)
-            print (lz4.dumps(json.dumps(val)),)
-            return '.z'+lz4.dumps(json.dumps(val))
+            print('========================')
+            print((json.dumps(val),))
+            print(lz4.dumps(json.dumps(val)),)
+            return '.z' + lz4.dumps(json.dumps(val))
 
         def loads(self, val):
             if val[:2] == '.z':
