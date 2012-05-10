8d47c5c58084be56b939b61159a9e665c9f4cbef
Nikolay Kim <fafhrd91@gmail.com>
2012-05-10 17:04:27 -0500

    pin simplejson

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ if sys.version_info[:2] == (2, 6):
             'unittest2'))
 
 if sys.version_info[:2] in ((2,6),(2,7)):
-    install_requires.extend(('simplejson',))
+    install_requires.extend(('simplejson == 2.5.0',))
 
 tests_require = install_requires + ['nose', 'sphinx', 'Pygments']
 
