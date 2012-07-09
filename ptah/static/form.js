5de8774070bf0f23ceff6093758e55c8b27a93e3
Nikolay Kim <fafhrd91@gmail.com>
2012-07-09 12:29:44 -0500

    fix file upload js

--- ptah/static/form.js
+++ ptah/static/form.js
@@ -78,33 +78,37 @@ define (
                     var that = this
 
                     var process_files = function() {
-                        var f = form_files[idx]
-                        var name = f.name
-                        idx += 1
                         if (idx >= form_files.length) {
                             that.connect.send(that.mtype, data)
                             return
                         }
 
+                        var f = form_files[idx]
+
+                        idx += 1
+
                         if (f.files.length) {
                             var fidx = 0
+                            var name = f.name
+
                             var process_file = function(files) {
-                                var file = files[fidx];
-                                var reader = new FileReader();
+                                var file = files[fidx]
+                                var reader = new FileReader()
                                 reader.onload = function (ev) {
                                     data.push([name+'-filename', file.name])
                                     data.push([name+'-mimetype', file.type])
                                     data.push([name, reader.result])
 
                                     fidx += 1
-                                    if (fidx < files.length) {
+                                    if (fidx < files.length)
                                         process_file(files)
-                                    } else {
+                                    else
                                         process_files()
-                                    }
                                 }
                                 reader.readAsBinaryString(file)
                             }
+
+                            process_file(f.files)
                         }
                     }
 
