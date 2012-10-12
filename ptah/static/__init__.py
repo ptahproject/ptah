7fbb8988085e100cca250e04bdbe0127d1db19ad
Nikolay Kim <fafhrd91@gmail.com>
2012-10-11 21:29:05 -0700

    use latest pyramid_amdjs api

--- ptah/static/__init__.py
+++ ptah/static/__init__.py
@@ -3,44 +3,44 @@ import ptah
 
 def includeme(config):
     # amd modules
-    config.add_amd_module(
+    config.add_amd_js(
         'jquery-ui', 'ptah:static/jquery/jquery-ui-1.9.0.min.js',
         'JQuery UI Library', ('jquery',))
-    config.add_amd_module(
+    config.add_amd_css(
         'jquery-ui-css', 'ptah:static/jquery/jquery-ui-1.9.0.min.css',
         'JQuery UI Library')
 
-    config.add_amd_module(
+    config.add_amd_js(
         'sockjs', 'ptah:static/lib/sockjs-0.3.2.min.js',
         'SockJS Client library')
-    config.add_amd_module(
+    config.add_amd_js(
         'bootstrap', 'ptah:static/bootstrap/bootstrap.min.js',
         'Twitter bootstrap javscript library', ('jquery',))
-    config.add_amd_module(
+    config.add_amd_css(
         'bootstrap-css', 
         'ptah:static/bootstrap/bootstrap.min.css',
         'Twitter bootstrap javscript library')
-    config.add_amd_module(
+    config.add_amd_css(
         'bootstrap-responsive-css',
         'ptah:static/bootstrap/bootstrap-responsive.min.css',
         'Twitter bootstrap javscript library (Responsive)')
 
-    config.add_amd_module(
+    config.add_amd_js(
         'ckeditor', 'ptah:static/ckeditor/ckeditor.js',
         'CKEditor', ('jquery',))
 
     # ptah
-    config.add_amd_module(
+    config.add_amd_js(
         'ptah', 'ptah:static/ptah.js', 'Ptah', ('handlebars','sockjs','jquery'))
-    config.add_amd_module(
+    config.add_amd_js(
         'ptah-date-format', 'ptah:static/date-format.js',
         require=('jquery', 'handlebars'))
 
     # ptah ui classes
-    config.add_amd_module(
+    config.add_amd_js(
         'ptah-form', 'ptah:static/form.js',
         require=('jquery', 'ptah', 'ptah-templates', 'bootstrap'))
-    config.add_amd_module(
+    config.add_amd_js(
         'ptah-pager', 'ptah:static/pager.js',
         require=('jquery', 'ptah', 'ptah-templates'))
 
