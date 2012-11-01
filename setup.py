bc06b48b8e8dc6a54e31ce350b4f81c5da153122
Nikolay Kim <fafhrd91@gmail.com>
2012-10-31 22:43:01 -0700

    increase version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import logging
 import multiprocessing  # atexit exception
 from setuptools import setup, find_packages
 
-version = '0.6.0dev'
+version = '0.8.0dev'
 
 install_requires = ['setuptools',
                     'alembic == 0.3.4',
