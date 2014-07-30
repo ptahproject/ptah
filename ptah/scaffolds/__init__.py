82be9e7f6db6bf003f9ae445337110d5e5f7d5ff
Marco Strobel <mail@strobel.ch>
2014-07-31 00:53:46 +0200

    Update pcreate output message. Add docs, examples, mailinglist, twitter informations

--- ptah/scaffolds/__init__.py
+++ ptah/scaffolds/__init__.py
@@ -1,12 +1,27 @@
+from textwrap import dedent
+
 from pyramid.scaffolds.template import Template
 
 
 class PtahTemplate(Template):
 
-    def post(self, command, output_dir, vars): # pragma: no cover
-        print ("\n\nPtah has generated your application. Your application "
-               "can be found at /. \n\nDorneles hopes you like it.\n")
 
+    def post(self, command, output_dir, vars): # pragma: no cover
+        separator = "=" * 79
+        msg = dedent(
+            """
+            %(separator)s
+            Examples: https://github.com/ptahproject/examples
+            Documentation: https://ptahproject.readthedocs.org
+
+            Mailing List: https://groups.google.com/forum/#!forum/ptahproject
+            Twitter: https://twitter.com/ptahproject
+
+            Welcome to Ptah. Dorneles hopes you like it.
+            %(separator)s
+        """ % {'separator': separator})
+
+        self.out(msg)
         return super(PtahTemplate, self).post(command, output_dir, vars)
 
 
