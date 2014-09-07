b2d720553092656cc1292f535e6aeb10322cad04
Marco Strobel <mail@strobel.ch>
2014-09-08 01:19:52 +0200

    move layout tests to scaffold

--- ptah/tests/test_view.py
+++ ptah/tests/test_view.py
@@ -22,14 +22,3 @@ class TestView(PtahTestCase):
     def test_view_update(self):
         view = ptah.View(Context(), self.request)
         self.assertEqual(view.update(), {})
-
-
-class TestMasterLayout(PtahTestCase):
-
-    def test_master_layout(self):
-        from ptah.view import MasterLayout
-
-        ml = MasterLayout(object(), self.request)
-        self.assertIsNone(ml.user)
-        self.assertEqual(ml.manage_url, 'http://example.com/ptah-manage')
-        self.assertEqual(ml.actions, [])
