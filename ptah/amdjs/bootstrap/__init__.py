de323ad7388cc70d7052b7c6114ff796e893f624
Marco Strobel <mail@strobel.ch>
2014-11-22 17:15:25 +0100

    update bootstrap 3.3.1

--- ptah/amdjs/bootstrap/__init__.py
+++ ptah/amdjs/bootstrap/__init__.py
@@ -6,17 +6,17 @@ def includeme(config):
 
     # static assets
     config.add_static_view('_amdjs_bootstrap/static', 'ptah.amdjs.bootstrap:static/')
-    
+
     config.add_amd_js(
         'bootstrap',
-        'ptah.amdjs.bootstrap:static/bootstrap-3.2.0/js/bootstrap.min.js',
+        'ptah.amdjs.bootstrap:static/dist/js/bootstrap.min.js',
         'Bootstrap Javascript Library', ('jquery',))
     config.add_amd_css(
         'bootstrap-css',
-        'ptah.amdjs.bootstrap:static/bootstrap-3.2.0/css/bootstrap.min.css',
+        'ptah.amdjs.bootstrap:static/dist/css/bootstrap.min.css',
         'Bootstrap CSS Library')
     config.add_amd_css(
         'bootstrap-theme-css',
-        'ptah.amdjs.bootstrap:static/bootstrap-3.2.0/css/bootstrap-theme.min.css',
+        'ptah.amdjs.bootstrap:static/dist/css/bootstrap-theme.min.css',
         'Bootstrap CSS Theme Library)')
 
