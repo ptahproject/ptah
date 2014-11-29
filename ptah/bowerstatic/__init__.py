472cee338a5e5c3474ebc3f8fe1f23a9072213bf
Marco Strobel <mail@strobel.ch>
2014-11-29 09:58:08 +0100

    use bowerstatic local components

--- ptah/bowerstatic/__init__.py
+++ ptah/bowerstatic/__init__.py
@@ -1,39 +1,54 @@
-import bowerstatic
+from bowerstatic import (
+    Bower,
+    Error,
+    InjectorTween,
+    PublisherTween,
+)
+from pyramid.path import AssetResolver
 
 
-class tween_factory(object):
+class bowerstatic_tween_factory(object):
     def __init__(self, handler, registry):
         self.handler = handler
         self.registry = registry
 
     def __call__(self, request):
-        injector_handler = bowerstatic.InjectorTween(
-            self.registry.bower, self.handler)
-        publisher_handler = bowerstatic.PublisherTween(
-            self.registry.bower, injector_handler)
+        injector_handler = InjectorTween(
+            self.registry.bower,
+            self.handler)
+        publisher_handler = PublisherTween(
+            self.registry.bower,
+            injector_handler)
 
         return publisher_handler(request)
 
-def init_bowerstatic(config, path):
-    name = config.registry.settings['bowerstatic.components_name']
-    config.registry.bower.components(name, path)
+
+def init_static_components(config, name='components', path=None):
+    if not path:
+        path = 'ptah:bowerstatic/bower_components'
+    resolver = AssetResolver()
+    directory = resolver.resolve(path).abspath()
+    components = config.registry.bower.components(name, directory)
+    local = config.registry.bower.local_components('local', components)
+
+
+def add_static_component(config, path, version=None):
+    resolver = AssetResolver()
+    directory = resolver.resolve(path).abspath()
+    local = config.registry.bower._component_collections['local']
+    local.component(directory, version)
 
 
 def include(request, path_or_resource):
-    name = request.registry.settings['bowerstatic.components_name']
-    bower = request.registry.bower
-    components = bower._component_collections.get(name)
-    if not components:
-        raise bowerstatic.Error("Component collection '%s' missing." % name)
-    include = components.includer(request.environ)
+    local = request.registry.bower._component_collections['local']
+    include = local.includer(request.environ)
     include(path_or_resource)
 
 
 def includeme(config):
+    config.registry.bower = Bower()
 
-    config.registry.settings['bowerstatic.components_name'] = 'components'
-    config.registry.bower = bowerstatic.Bower()
-
-    config.add_directive('init_bowerstatic', init_bowerstatic)
-    config.add_tween('ptah.bowerstatic.tween_factory')
+    config.add_tween('ptah.bowerstatic.bowerstatic_tween_factory')
+    config.add_directive('init_static_components', init_static_components)
+    config.add_directive('add_static_component', add_static_component)
     config.add_request_method(include, 'include')
